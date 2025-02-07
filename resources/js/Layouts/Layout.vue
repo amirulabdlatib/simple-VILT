120e996d4d8b5523bb1fe712b5e72807efced84f
Amirul Abd Latib <amirul.latib@alphv.com>
2025-02-07 21:53:19 +0800

    Code Cleanup

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -10,12 +10,10 @@
     </Head>
 
     <div>
-        <header class="bg-indigo-500 text-white">
-            <nav class="flex items-center justify-between p-4 max-w-screen-lg mx-auto">
+        <header>
+            <nav>
                 <div class="space-x-6">
-                    <Link :href="route('home')">Home</Link>
-                    <Link :href="route('about')">About</Link>
-                    <Link :href="route('test')">Test</Link>
+                    <Link :href="route('home')" class="nav-link">Home</Link>
                 </div>
             </nav>
         </header>
--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,6 @@ Route::get('/', function () {
     return Inertia::render('Home');
 })->name('home');
 
-Route::get('/about',function(){
-    return inertia('About',['user' => 'Mike']);
-})->name('about');
-
-Route::get('/test',function(){
-    return Inertia::render('Test');
-})->name('test');
 
 // Alternative 
 // Route::inertia('/about','About',['user' => 'Mike']);
