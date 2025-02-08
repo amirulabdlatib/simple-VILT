3a5ff63881a38c820114dbf6d02d3f3359052d53
Amirul Abd Latib <amirul.latib@alphv.com>
2025-02-08 17:51:25 +0800

    Added middleware

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,15 @@ Route::get('/', function () {
     return Inertia::render('Home');
 })->name('home');
 
-Route::inertia('/register','Auth/Register')->name('register');
-Route::post('/register',[AuthController::class,'register']);
-Route::inertia('/dashboard','Dashboard')->name('dashboard');
 
-Route::inertia('/login','Auth/Login')->name('login');
-Route::post('/login',[AuthController::class, 'login']);
-Route::post('/logout',[AuthController::class, 'logout'])->name('logout');
-
-// Alternative 
-// Route::inertia('/about','About',['user' => 'Mike']);
+Route::middleware(['guest'])->group(function(){
+    Route::inertia('/register','Auth/Register')->name('register');
+    Route::post('/register',[AuthController::class,'register']);
+    Route::inertia('/login','Auth/Login')->name('login');
+    Route::post('/login',[AuthController::class, 'login']);
+});
+   
+Route::middleware(['auth'])->group(function(){
+    Route::inertia('/dashboard','Dashboard')->name('dashboard');
+    Route::post('/logout',[AuthController::class, 'logout'])->name('logout');
+});
