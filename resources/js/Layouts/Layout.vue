728670655f53948e0ce04823066a4388864bb9c0
Amirul Abd Latib <amirul.latib@alphv.com>
2025-02-08 17:26:33 +0800

    Implemented logout functionality

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -16,7 +16,10 @@
                     <Link :href="route('home')" class="nav-link">Home</Link>
                 </div>
 
-                <div class="space-x-6">
+                <div v-if="$page.props.auth.user" class="space-x-6">
+                    <Link :href="route('logout')" method="POST" as="button" type="button" class="nav-link">Logout</Link>
+                </div>
+                <div v-else class="space-x-6">
                     <Link :href="route('register')" class="nav-link">Register</Link>
                     <Link :href="route('login')" class="nav-link">Login</Link>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ Route::post('/register',[AuthController::class,'register']);
 
 Route::inertia('/login','Auth/Login')->name('login');
 Route::post('/login',[AuthController::class, 'login']);
+Route::post('/logout',[AuthController::class, 'logout'])->name('logout');
 
 // Alternative 
 // Route::inertia('/about','About',['user' => 'Mike']);
