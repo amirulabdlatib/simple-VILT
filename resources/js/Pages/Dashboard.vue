bc1482a8c7fec65f21cd7960711ff2ee52432673
Amirul Abd Latib <amirul.latib@alphv.com>
2025-02-09 10:12:31 +0800

    Added close functionality to flash message

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,5 +1,8 @@
 <script setup>
+import { ref } from 'vue'
 
+const showFlash = ref(true)
+const closeFlash = () => showFlash.value = false
 </script>
 
 <template>
@@ -8,8 +11,13 @@
         <title>Dashboard</title>
     </Head>
 
-    <p class="p-4 bg-green-200" v-if="$page.props.flash.message">
+    <p class="p-4 bg-green-200 relative" v-if="$page.props.flash.message && showFlash">
         {{ $page.props.flash.message }}
+        <button @click="closeFlash" class="absolute top-1 right-1 p-1 hover:bg-green-300 rounded">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </button>
     </p>
 
     <div>
