5e3f509f701e3726f655f22135c889af3fc4eb93
Amirul Abd Latib <amirul.latib@alphv.com>
2025-02-09 10:16:35 +0800

    Extracted Flash message as component

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,8 +1,6 @@
 <script setup>
-import { ref } from 'vue'
+import FlashMessage from './Components/FlashMessage.vue';
 
-const showFlash = ref(true)
-const closeFlash = () => showFlash.value = false
 </script>
 
 <template>
@@ -10,16 +8,7 @@ const closeFlash = () => showFlash.value = false
     <Head>
         <title>Dashboard</title>
     </Head>
-
-    <p class="p-4 bg-green-200 relative" v-if="$page.props.flash.message && showFlash">
-        {{ $page.props.flash.message }}
-        <button @click="closeFlash" class="absolute top-1 right-1 p-1 hover:bg-green-300 rounded">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-        </button>
-    </p>
-
+    <FlashMessage/>
     <div>
         <h1>Welcome back {{ $page.props.auth.user ? $page.props.auth.user.name : 'Guest' }}</h1>
     </div>
