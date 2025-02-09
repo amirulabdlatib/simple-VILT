ede79eb3b72ffee4995595cb5f0acdfe7604f7fd
Amirul Abd Latib <amirul.latib@alphv.com>
2025-02-09 10:58:33 +0800

    Finished index page

--- resources/js/Pages/Tasks/Index.vue
+++ resources/js/Pages/Tasks/Index.vue
@@ -1,4 +1,15 @@
 <script setup>
+defineProps({
+    tasks:Object,
+})
+
+function formateDate(date) {
+    return new Date(date).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    })
+}
 
 </script>
 
@@ -8,7 +19,27 @@
         <title>Task</title>
     </Head>
 
-    <div>
+    <div class="my-3">
         <h1>My Tasks</h1>
     </div>
+
+    <table>
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Registered Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(task, index) in tasks" :key="task.id">
+                <td>{{ index + 1 }}</td>
+                <td>{{ task.name }}</td>
+                <td>{{ task.description }}</td>
+                <td>{{ formateDate(task.created_at) }}</td>
+            </tr>
+        </tbody>
+    </table>
+
 </template>
